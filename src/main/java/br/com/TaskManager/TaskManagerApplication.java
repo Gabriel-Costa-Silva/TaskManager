78a0fb006514d25d33d695d071311e3d3e2f0387
Gabriel-Costa-Silva <gabrielcostasilva.1918@gmail.com>
2022-10-10 22:22:51 -0300

    Modificando alguns parâmetros de inicialização

--- src/main/java/br/com/TaskManager/TaskManagerApplication.java
+++ src/main/java/br/com/TaskManager/TaskManagerApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TaskManagerApplication {
+public class  TaskManagerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaskManagerApplication.class, args);
