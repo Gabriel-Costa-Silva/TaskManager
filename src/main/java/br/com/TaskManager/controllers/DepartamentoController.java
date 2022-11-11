f784517286a8103836954d2ed1cf71397ecbaaac
Gabriel-Costa-Silva <90655101+Gabriel-Costa-Silva@users.noreply.github.com>
2022-11-11 23:14:07 +0000

    teste Github editor

--- src/main/java/br/com/TaskManager/controllers/DepartamentoController.java
+++ src/main/java/br/com/TaskManager/controllers/DepartamentoController.java
@@ -1,9 +1,21 @@
 package br.com.TaskManager.controllers;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RestController
+@RequestMapping("/api/departamento")
 public class DepartamentoController {
+    DepartamentoService departamentoService;
     //get
+    @GetMapping
+    List<Departamento> getDepartamentos(){
+        departamentoService.getDepartamentos();
+    }
 
     //findById
+    @GetMapping()
+    Departamento getDepartamentoById(){
+
+    }s
 
     //save
 
